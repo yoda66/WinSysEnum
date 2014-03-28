ae28115c1cf980762eeb308d83274d662b0643b1
jsthyer <jsthyer@gmail.com>
2014-03-28 12:27:03 -0400

    modified command set to include domain info

--- WinSysEnum.cmd
+++ WinSysEnum.cmd
@@ -4,11 +4,9 @@ FOR /F "tokens=2 delims='='" %%A in ('wmic os get csname /value') do SET csname=
 
 call:datetime
 set out=%csname%-%DT%.txt
-set version=V20140205-0930
+set version=V20140328-0930
 echo [*] Windows System Analysis Script %version%
 echo [*] Output file is [%CD%\%out%]
-echo [*] CTRL-C to abort...
-pause
 
 echo [*] %DT%: Writing output to [%out%]
 echo [*] ===================================================== >%out%
@@ -16,65 +14,54 @@ echo [*] Windows System Analysis Script %version% >>%out%
 echo [*] %DT%: Gathering Information from [%csname%] >>%out%
 echo [*] ===================================================== >>%out%
 
-call:runcmd %out% "arp -a"
-call:runcmd %out% "ipconfig /all"
-call:runcmd %out% "ipconfig /displaydns"
-call:runcmd %out% "systeminfo"
-call:runcmd %out% "net user"
-call:runcmd %out% "net accounts"
-call:runcmd %out% "net view /domain"
-call:runcmd %out% "net use"
-call:runcmd %out% "net session"
-call:runcmd %out% "net localgroup administrators"
-call:runcmd %out% "net time /querysntp"
+call:runcmd %out% arp -a
+call:runcmd %out% ipconfig /all
+call:runcmd %out% ipconfig /displaydns
+call:runcmd %out% net user
+call:runcmd %out% net accounts
+call:runcmd %out% net view /domain
+call:runcmd %out% net use
+call:runcmd %out% net session
+call:runcmd %out% net localgroup administrators
+call:runcmd %out% net group "Domain Admins" /domain
+call:runcmd %out% net group "Domain Controllers" /domain
+call:runcmd %out% net time /querysntp
 
-call:runcmd %out% "arp -a"
-call:runcmd %out% "netsh firewall show state"
-call:runcmd %out% "netsh firewall show config"
-call:runcmd %out% "netsh firewall show logging"
-call:runcmd %out% "netsh firewall show opmode"
-call:runcmd %out% "netsh firewall show portopening"
+call:runcmd %out% netsh firewall show state
+call:runcmd %out% netsh firewall show config
+call:runcmd %out% netsh firewall show logging
+call:runcmd %out% netsh firewall show opmode
+call:runcmd %out% netsh firewall show portopening
 
-call:runcmd %out% "netstat -nr"
-call:runcmd %out% "netstat -ano"
-call:runcmd %out% "netstat -banov"
+call:runcmd %out% netstat -nr
+call:runcmd %out% netstat -naop
+call:runcmd %out% netstat -banov
 
-call:runcmd %out% "arp -a"
-call:runcmd %out% "nslookup microsoft.com"
-call:runcmd %out% "nslookup google.com"
-call:runcmd %out% "nslookup microsoft.com 8.8.8.8"
-call:runcmd %out% "nslookup www.opendns.com 8.8.8.8"
+call:runcmd %out% sc query
+call:runcmd %out% schtasks
 
-call:runcmd %out% "arp -a"
-call:runcmd %out% "tracert /w 1000 8.8.8.8"
-call:runcmd %out% "ping microsoft.com"
-call:runcmd %out% "ping 8.8.8.8"
-call:runcmd %out% "sc query"
-call:runcmd %out% "schtasks"
-call:runcmd %out% "arp -a"
+call:runcmd %out% reg query HKLM\Software\Microsoft\Windows\CurrentVersion\Run
+call:runcmd %out% reg query HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce
+call:runcmd %out% reg query HKLM\Software\Microsoft\Windows NT\CurrentVersion\ProfileList
+call:runcmd %out% reg query HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall
+call:runcmd %out% reg query HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU
+call:runcmd %out% reg query HKLM\SYSTEM\CurrentControlSet\Enum\USBSTOR
+call:runcmd %out% reg query HKLM\SYSTEM\CurrentControlSet\Enum\USB
+call:runcmd %out% reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2
+call:runcmd %out% reg query HKLM\Software\RealVNC\WinVNC4
 
-call:runcmd %out% "reg query HKLM\Software\Microsoft\Windows\CurrentVersion\Run"
-call:runcmd %out% "reg query HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce"
-call:runcmd %out% "reg query HKLM\Software\Microsoft\Windows NT\CurrentVersion\ProfileList"
-call:runcmd %out% "reg query HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall"
-call:runcmd %out% "reg query HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU"
-call:runcmd %out% "reg query HKLM\SYSTEM\CurrentControlSet\Enum\USBSTOR"
-call:runcmd %out% "reg query HKLM\SYSTEM\CurrentControlSet\Enum\USB"
-call:runcmd %out% "reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2"
-call:runcmd %out% "reg query HKLM\Software\RealVNC\WinVNC4"
-
-call:runcmd %out% "wmic product get name, version"
-call:runcmd %out% "wmic environment list"
-call:runcmd %out% "wmic group list"
-call:runcmd %out% "wmic nicconfig list"
-call:runcmd %out% "wmic qfe list full"
-call:runcmd %out% "wmic qfe list"
-call:runcmd %out% "wmic share list"
-call:runcmd %out% "wmic nicconfig list"
-call:runcmd %out% "wmic useraccount list"
-call:runcmd %out% "wmic useraccount list brief"
-call:runcmd %out% "wmic useraccount list full"
-call:runcmd %out% "wmic sysaccount list"
+call:runcmd %out% wmic product get name, version
+call:runcmd %out% wmic environment list
+call:runcmd %out% wmic group list
+call:runcmd %out% wmic nicconfig list
+call:runcmd %out% wmic qfe list full
+call:runcmd %out% wmic qfe list
+call:runcmd %out% wmic share list
+call:runcmd %out% wmic nicconfig list
+call:runcmd %out% wmic useraccount list
+call:runcmd %out% wmic useraccount list brief
+call:runcmd %out% wmic useraccount list full
+call:runcmd %out% wmic sysaccount list
 
 call:datetime
 echo [*] ==========================================
@@ -87,9 +74,9 @@ goto:exitnow
 
 :runcmd
 call:datetime
-echo [*] %DT%: %~2
-echo [*] %DT%: %~2 >>%1 2>&1
-%~2 >>%1 2>&1
+echo [*] %DT%: %2 %3 %4 %5 %6 %7 %8 %9
+echo [*] %DT%: %2 %3 %4 %5 %6 %7 %8 %9 >>%1 2>&1
+%2 %3 %4 %5 %6 %7 %8 %9 >>%1 2>&1
 goto:eof
 
 
